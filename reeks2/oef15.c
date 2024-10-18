313a816f823d7794c047197f29cc2cb687a99a19
tim.billiet <tim.billiet@student.odisee.be>
2024-10-19 00:18:02 +0200

    oef15 verticaal

--- reeks2/oef15.c
+++ reeks2/oef15.c
@@ -3,27 +3,26 @@
 void lees_input(int frequenties[]);
 void teken_horizontaal(int frequenties[]);
 void teken_verticaal(int frequenties[]);
-
+char hoofdletter_naar_kleine(char letter);
+int meest_aantal_letters(int frequenties[]);
 int main() {
     int frequenties[26] = {0};
     lees_input(frequenties);
-    // for (int i = 0; i < 26; i++) {
-    //     printf("%c, ", (char)i + (int)'a');
-    // }
-    // printf("\n");
-    // for (int i = 0; i < sizeof(frequenties) / sizeof(frequenties[0]); i++) {
-    //     printf("%d, ", frequenties[i]);
-    // }
     teken_horizontaal(frequenties);
     teken_verticaal(frequenties);
 
     return 0;
 }
+
+char hoofdletter_naar_kleine(char letter) {
+    return (char)(int)letter + ((int)'a' - (int)'A');
+}
+
 void lees_input(int frequenties[]) {
     char input_char;
     while (scanf("%c", &input_char) == 1 && input_char != '$') {
         if ((int)input_char >= (int)'A' && (int)input_char <= (int)'Z') {  // is de letter een hoofdletter
-            input_char = (char)(int)input_char + ((int)'a' - (int)'A');    // maak van de hoofdletter een kleine letter
+            input_char = hoofdletter_naar_kleine(input_char);              // maak van de hoofdletter een kleine letter
         }
         frequenties[(int)input_char - (int)'a']++;
     }
@@ -37,5 +36,27 @@ void teken_horizontaal(int frequenties[]) {
         printf("\n");
     }
 }
+int meest_aantal_letters(int frequenties[]) {
+    int grootste = 0;
+    for (int i = 0; i < 26; i++) {
+        if (frequenties[i] > grootste) {
+            grootste = frequenties[i];
+        }
+    }
+    return grootste;
+}
 void teken_verticaal(int frequenties[]) {
+    printf("max voorkomend = %d\n", meest_aantal_letters(frequenties));
+
+    for (int i = meest_aantal_letters(frequenties); i > 0; i--) {
+        for (int j = 0; j < 26; j++) {
+            if (frequenties[j] == i) {
+                frequenties[j]--;
+                printf("%c", j + (int)'a');
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 }
