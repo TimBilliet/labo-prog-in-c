89a2cb8a7ace0f566d0f7940fa45ea949a4ab603
Tim Billiet <tim.billiet@student.odisee.be>
2024-11-22 11:20:30 +0100

    oef29 af & werkend

--- reeks4/oef29.c
+++ reeks4/oef29.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 char* alfab_kleinste(char**, int);
 void my_toupper(char*);
@@ -7,19 +8,18 @@ int main(int argc, char** argv) {
     if (argc == 1) {
         printf("Dag allemaal!\n");
     } else {
-        char* alfab_kleinste_naam = alfab_kleinste(argv, sizeof(argv) / sizeof(argv[0]));
+        char* alfab_kleinste_naam = alfab_kleinste(argv, argc);
         my_toupper(alfab_kleinste_naam);
         printf("Dag %s!\n", alfab_kleinste_naam);
     }
-
     return 0;
 }
 
 char* alfab_kleinste(char** voornamen, int n) {
-    char* kleinste = *voornamen;
-    for (int i = 0; i < n; i++) {
-        if (**voornamen > *kleinste) {  // of *voornamen[i] > kleinste[i]
-            kleinste = *voornamen;
+    char* kleinste = voornamen[0];
+    for (int i = 1; i < n; i++) {
+        if (strcmp(voornamen[i], kleinste) < 0) {
+            kleinste = voornamen[i];
         }
     }
     return kleinste;
