1f36e1d86c072be68bd3eca043aa4a460919da28
Tim Billiet <tim.billiet@student.odisee.be>
2024-11-28 13:25:05 +0100

    aanmaken oef30

---
+++ reeks5/oef30.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+    double x;
+    double y
+} punt;
+
+int main() {
+    return 0;
+}
+
+double euclidische_afstand() {
+}
+
+double manhattan_afstand() {
+}
+double schaakbord_afstand() {
+}
