c53ed17a61c81bea1731eecba9590fe1292afefb
Tim Billiet <tim.billiet@student.odisee.be>
2024-11-29 11:06:10 +0100

    stukje oef30

--- reeks5/oef30.c
+++ reeks5/oef30.c
@@ -8,11 +8,11 @@ typedef struct {
 double euclidische_afstand(const punt*, const punt*);
 double manhattan_afstand(const punt*, const punt*);
 double schaakbord_afstand(const punt*, const punt*);
-
+double bepaal_max_afstand(const punt*[], int, double (*)(const punt*, const punt*));
 int main() {
     punt p1 = {
-        .x = 2,
-        .y = 3,
+        .x = 7,
+        .y = 8,
     };
 
     punt p1 = {
@@ -23,6 +23,17 @@ int main() {
     return 0;
 }
 
+double bepaal_max_afstand(const punt* punten[], int aantal, double (*func)(const punt*, const punt*)) {
+    int startpos = 0;
+
+    while (startpos < aantal) {
+        // for (int i = startpos; i < aantal; i++) {
+        //     punt* eerst = *punten++;
+        //     double afstand = func(eerst, *punten);
+        // }
+    }
+}
+
 double euclidische_afstand(const punt* p1, const punt* p2) {
     return sqrt(pow(fabs(p1->x - p2->x), 2) + pow(fabs(p1->y - p2->y), 2));
 }
@@ -30,6 +41,7 @@ double euclidische_afstand(const punt* p1, const punt* p2) {
 double manhattan_afstand(const punt* p1, const punt* p2) {
     return (fabs(p1->x - p2->x)) + (fabs(p1->y - p2->y));
 }
+
 double schaakbord_afstand(const punt* p1, const punt* p2) {
-    return
+    return max(fabs(p1->x - p2->x), fabs(p1->y - p2->y));
 }
