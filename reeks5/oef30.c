0f253ac4825a1607ecfbf360da9dda186a78a326
Tim Billiet <tim.billiet@student.odisee.be>
2024-12-06 11:03:38 +0100

    fix oef 30 vervanging van array van punt pointers naar gewoon array van punten

--- reeks5/oef30.c
+++ reeks5/oef30.c
@@ -9,7 +9,7 @@ typedef struct {
 double euclidische_afstand(const punt*, const punt*);
 double manhattan_afstand(const punt*, const punt*);
 double schaakbord_afstand(const punt*, const punt*);
-double bepaal_max_afstand(const punt*[], int, double (*)(const punt*, const punt*));
+double bepaal_max_afstand(const punt[], int, double (*)(const punt*, const punt*));
 double max(double, double);
 int main() {
     // punt p1 = {
@@ -39,46 +39,43 @@ int main() {
         while (getchar() != '\n');
         printf("Foute invoer, voer een nieuw getal in\n");
     }
-    punt* ingevoerde_punten[aantal_punten];
+    const punt ingevoerde_punten[aantal_punten];
 
     for (int i = 0; i < aantal_punten; i++) {
-        ingevoerde_punten[i] = malloc(sizeof(punt));
-        double x = 0;
+        // ingevoerde_punten[i] = malloc(sizeof(punt));
+        // double x = 0;
         printf("Vul een x-coordinaat in voor punt %d\n", i + 1);
-        while (scanf("%lf", &x) != 1) {
+        while (scanf("%lf", &ingevoerde_punten[i].x) != 1) {
             while (getchar() != '\n');
             printf("Foute invoer, voer een nieuw x-coordinaat in\n");
         }
         printf("Vul een y-coordinaat in voor punt %d\n", i + 1);
-        double y = 0;
-        while (scanf("%lf", &y) != 1) {
+        // double y = 0;
+        while (scanf("%lf", &ingevoerde_punten[i].y) != 1) {
             while (getchar() != '\n');
             printf("Foute invoer, voer een nieuw y-coordinaat in\n");
         }
-        ingevoerde_punten[i]->x = x;
-        ingevoerde_punten[i]->y = y;
+        // ingevoerde_punten[i].x = x;
+        // ingevoerde_punten[i].y = y;
     }
-    printf("Max euclidische afstand is %f\n", bepaal_max_afstand((const punt**)ingevoerde_punten, sizeof(ingevoerde_punten) / sizeof(ingevoerde_punten[0]), euclidische_afstand));
-    printf("Max manhattan afstand is %f\n", bepaal_max_afstand((const punt**)ingevoerde_punten, sizeof(ingevoerde_punten) / sizeof(ingevoerde_punten[0]), manhattan_afstand));
-    printf("Max schaakbordafstand is %f\n", bepaal_max_afstand((const punt**)ingevoerde_punten, sizeof(ingevoerde_punten) / sizeof(ingevoerde_punten[0]), schaakbord_afstand));
-    for (int i = 0; i < aantal_punten; i++) {
-        free(ingevoerde_punten[i]);
-    }
+    printf("Max euclidische afstand is %f\n", bepaal_max_afstand((const punt*)ingevoerde_punten, sizeof(ingevoerde_punten) / sizeof(ingevoerde_punten[0]), euclidische_afstand));
+    printf("Max manhattan afstand is %f\n", bepaal_max_afstand((const punt*)ingevoerde_punten, sizeof(ingevoerde_punten) / sizeof(ingevoerde_punten[0]), manhattan_afstand));
+    printf("Max schaakbordafstand is %f\n", bepaal_max_afstand((const punt*)ingevoerde_punten, sizeof(ingevoerde_punten) / sizeof(ingevoerde_punten[0]), schaakbord_afstand));
     return 0;
 }
 
-double bepaal_max_afstand(const punt* punten[], int aantal, double (*func)(const punt*, const punt*)) {
-    const punt** einde = punten + aantal;
-    const punt** startpos = punten;
+double bepaal_max_afstand(const punt punten[], int aantal, double (*func)(const punt*, const punt*)) {
+    const punt* einde = punten + aantal;
+    const punt* startpos = punten;
     double afstand = 0;
     while (startpos < einde) {
-        const punt* eerste_punt = *startpos;
+        const punt eerste_punt = *startpos;
         punten = startpos;
         punten++;
 
         while (punten < einde) {
-            const punt* tweede_punt = *punten++;
-            afstand = max(afstand, func(eerste_punt, tweede_punt));
+            const punt tweede_punt = *punten++;
+            afstand = max(afstand, func(&eerste_punt, &tweede_punt));
             // printf("vergelijking tussen %f x en %f x\n", eerste_punt->x, tweede_punt->x);
         }
         startpos++;
