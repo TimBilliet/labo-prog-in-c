3654554a38a6d4b102cdd7b5da663fc563072bbb
Tim Billiet <tim.billiet@student.odisee.be>
2024-12-06 10:31:10 +0100

    oef31 af

--- reeks5/oef31.c
+++ reeks5/oef31.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 char* lees();
-int aantal = 1000;
+int aantal = 5;
 int main() {
     for (int i = 0; i < 5; i++) {
         printf("Voer een tekst in\n");
@@ -14,10 +14,16 @@ int main() {
 }
 
 char* lees() {
-    char str_in[aantal];
-    fgets(str_in, aantal, stdin);
-    int len = strlen(str_in) - 1;
-    char* kleiner = malloc(sizeof(char) * len);
-    strncpy(kleiner, str_in, len);
+    char str_in[aantal + 1];
+    fgets(str_in, aantal + 1, stdin);
+    int len = strlen(str_in);  // len is stringlengte zonder nullchar
+    if (str_in[len - 1] == '\n') {
+        str_in[len - 1] = '\0';
+        len--;
+    } else {
+        while (getchar() != '\n');
+    }
+    char* kleiner = malloc(sizeof(char) * len + 1);
+    strcpy(kleiner, str_in);
     return kleiner;
 }
